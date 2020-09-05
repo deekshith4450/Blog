1d71773790c179821e1b7979d81e83cd61ad4a8b
deekshith4450 <deekshith4450@gmail.com>
2020-09-05 14:20:24 -0500

    build

---
+++ precache-manifest.22d9a75b8188733b5a1539a14698f57a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa01e82a30d4eee8f855f92b78488397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4acf3024c93e30d68abb",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "5fd43a56c81c5e420163",
+    "url": "/static/css/main.67093429.chunk.css"
+  },
+  {
+    "revision": "4acf3024c93e30d68abb",
+    "url": "/static/js/2.20da3471.chunk.js"
+  },
+  {
+    "revision": "e37c3d8d3061b639cf56486e4106bf09",
+    "url": "/static/js/2.20da3471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd43a56c81c5e420163",
+    "url": "/static/js/main.d206aa39.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
